751525e934aab4842d6e2f5ecb6c817db17ab0c4
Doruk Delicay <mehmetdorukdelicay01@gmail.com>
2026-03-19 13:49:45 +0300

    Fix guess count label and show answer warning

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ title.pack(pady=10)
 rules = tk.Label(window, text="Between 1-100", font=("Arial", 12))
 rules.pack(pady=10)
 
-guessPcs = tk.Label(window, text="Guess Number: 0", font=("Arial", 11))
+guessPcs = tk.Label(window, text="Number of Guesses: 0", font=("Arial", 11))
 guessPcs.pack(pady=10)
 
 hint = tk.Label(window, text="", fg="red", font=("Arial", 11))
@@ -36,12 +36,14 @@ def main():
         subprocess.call("cls", shell=True)
 
         guesspcsnumber = 0
-        guessPcs.config(text="Guess Number: 0")
+        guessPcs.config(text="Number of Guesses: 0")
         result.config(text="PLAYING...", fg="black", font=("Arial", 11))
         hint.config(text="")
 
     def show():
         print(randomNumber)
+
+        messagebox.showwarning("Alert","Answer Displayed in Shell")
 
     def guess():
         def go():
@@ -53,13 +55,13 @@ def main():
                 return
 
             guesspcsnumber += 1
-            guessPcs.config(text="Guess Number: " + str(guesspcsnumber))
+            guessPcs.config(text="Number of Guesses: " + str(guesspcsnumber))
 
             if guessNumber == randomNumber:
                 result.config(text="CORRECT GUESS!", fg="green", font=("Arial", 13))
                 hint.config(text="")
 
-                messagebox.showinfo("Correct", ("Guess Number: " + str(guesspcsnumber)))
+                messagebox.showinfo("Correct", ("Number of Guesses: " + str(guesspcsnumber)))
 
             elif guessNumber < randomNumber:
                 hint.config(text="Guess A Larger Number")
